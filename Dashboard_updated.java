1679c79dbbc65e30bd038dfa29ee967b824af338
rs-srishti <srishtirubaiya@gmail.com>
2025-12-05 14:55:43 +0600

    Updated dashboard

---
+++ Dashboard_updated.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Dashboard extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 900, 600);
+
+        scene.getStylesheets().add(getClass().getResource("dashboard_style.css").toExternalForm());
+
+        stage.setTitle("User Dashboard");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
